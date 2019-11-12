1ef6aa3765cb1fb8673891db8cc03fbc02050ee6
Dmitry Dokuchaev <alllok95@gmail.com>
2019-11-13 00:48:38 +0700

    fix(logger): Изменить уровень логгирования

--- app/logger.py
+++ app/logger.py
@@ -7,10 +7,10 @@ __all__ = ['logger']
 
 
 logger = logging.getLogger('app')
-
+logger.setLevel(logging.DEBUG)
 
 _LOG_PATH = config['log_path']
-_LOG_FORMAT = '%(asctime)s [%(levelname)s] %(filename)s: %(message)s'
+_LOG_FORMAT = '%(asctime)s [%(levelname)s] %(filename)s : %(message)s'
 
 # Создание директории и файла, если они не существуют
 if not os.path.exists(_LOG_PATH):
@@ -24,13 +24,17 @@ _FILE_HANDLER = logging.FileHandler(_LOG_PATH)
 _STREAM_HANDLER = logging.StreamHandler()
 
 # LEVEL configuring
-_FILE_HANDLER.setLevel(logging.WARNING)
-_STREAM_HANDLER.setLevel(logging.WARNING)
+_FILE_HANDLER.setLevel(logging.DEBUG)
+_STREAM_HANDLER.setLevel(logging.DEBUG)
+
+# Create formatter
+_FILE_FORMATTER = logging.Formatter(_LOG_FORMAT)
+_STREAM_FORMATTER = logging.Formatter(_LOG_FORMAT)
 
 # SET FORMATTER
-_FILE_HANDLER.setFormatter(logging.Formatter(_LOG_FORMAT))
-_STREAM_HANDLER.setFormatter(logging.Formatter(_LOG_FORMAT))
+_FILE_HANDLER.setFormatter(_FILE_FORMATTER)
+_STREAM_HANDLER.setFormatter(_STREAM_FORMATTER)
 
 # ADD HANDLERS
-logger.addHandler(_FILE_HANDLER)
 logger.addHandler(_STREAM_HANDLER)
+logger.addHandler(_FILE_HANDLER)
