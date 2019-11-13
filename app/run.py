2075775f9310fef0fc5828a254b9a9e0d4fd063f
Dmitry Dokuchaev <alllok95@gmail.com>
2019-11-14 02:13:54 +0700

    feat(log): Реализовать предв. лог результата

--- app/run.py
+++ app/run.py
@@ -8,8 +8,13 @@ def main():
 
     reader = GenomesReader('genomes/')
     for sequence in reader:
+        logger.info(f'Исследование последовательности: {sequence.name}')
         dispatcher = SubSeqDispatcher(sequence.seq)
         result: HighestSubSeqInfo = dispatcher.highest_sub_seq()
+        if result:
+            logger.info(f'Наибольшая подпоследовательность длиной {result.length}'
+                        f'последовательность начинается с индекса №{result.start_index}'
+                        f'первый повтор начинается с индекса №{result.repeat_start_index}')
 
     logger.debug('Process was done')
 
